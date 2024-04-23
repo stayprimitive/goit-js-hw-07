ca1de061e978092d8260d6d423165ddb81814936
stayprimitive <119599068+stayprimitive@users.noreply.github.com>
2024-04-24 01:54:16 +0300

    Task-4 - DONE!

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", fuSubmit);
+
+function fuSubmit(event) {
+    event.preventDefault();
+    const elements = event.target.elements;
+    const emailInput = elements.email.value.trim();
+    const passwordInput = elements.password.value.trim();
+
+    const result = {
+        email: emailInput,
+        password: passwordInput,
+    }
+
+    if (emailInput.length < 1 || passwordInput < 1) {
+        alert("All form fields must be filled in!");
+    }
+
+    console.log(result);
+    event.target.reset();
+}
 

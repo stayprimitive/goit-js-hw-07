017d64abd64cbddac29a21b3868d5699273530b2
stayprimitive <119599068+stayprimitive@users.noreply.github.com>
2024-05-09 02:03:17 +0300

    edited task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,44 +1,52 @@
-const categories = document.querySelectorAll("li.item");
+// const categories = document.querySelectorAll("li.item");
 
 
-let categoriesArray = [];
-categories.forEach(function (category) {
-    categoriesArray.push(category);
+// let categoriesArray = [];
+// categories.forEach(function (category) {
+//     categoriesArray.push(category);
+// });
+// let categoriesArrayResult = "Number of categories: " + categoriesArray.length;
+// console.log(categoriesArrayResult);
+
+
+// // One // 
+// const categoryOne = categories[0].querySelector("h2").textContent;
+// const categoryOneUl = categories[0].querySelectorAll("li");
+// let categoryOneArray = [];
+// categoryOneUl.forEach(function (category) {
+//     categoryOneArray.push(category);
+// });
+// let categoryOneArrayResult = "Elements: " + categoryOneArray.length;
+// console.log("Category: " + categoryOne);
+// console.log(categoryOneArrayResult);
+
+// // Two // 
+// const categoryTwo = categories[1].querySelector("h2").textContent;
+// const categoryTwoUl = categories[1].querySelectorAll("li");
+// let categoryTwoArray = [];
+// categoryTwoUl.forEach(function (category) {
+//     categoryTwoArray.push(category);
+// });
+// let categoryTwoArrayResult = "Elements: " + categoryTwoArray.length;
+// console.log("Category: " + categoryTwo);
+// console.log(categoryTwoArrayResult);
+
+// // Three // 
+// const categoryThree = categories[2].querySelector("h2").textContent;
+// const categoryThreeUl = categories[2].querySelectorAll("li");
+// let categoryThreeArray = [];
+// categoryThreeUl.forEach(function (category) {
+//     categoryThreeArray.push(category);
+// });
+// let categoryThreeArrayResult = "Elements: " + categoryThreeArray.length;
+// console.log("Category: " + categoryThree);
+// console.log(categoryThreeArrayResult);
+
+const categoriesEl = document.getElementById('categories').children.length;
+console.log(`Number of categories: ${categoriesEl}`);
+
+const arrayTitlesCategoriesRef = document.querySelectorAll('h2');
+arrayTitlesCategoriesRef.forEach((title) => {
+  console.log('Category:', title.textContent);
+  console.log('Elements:', title.nextElementSibling.children.length);
 });
-let categoriesArrayResult = "Number of categories: " + categoriesArray.length;
-console.log(categoriesArrayResult);
-
-
-// One // 
-const categoryOne = categories[0].querySelector("h2").textContent;
-const categoryOneUl = categories[0].querySelectorAll("li");
-let categoryOneArray = [];
-categoryOneUl.forEach(function (category) {
-    categoryOneArray.push(category);
-});
-let categoryOneArrayResult = "Elements: " + categoryOneArray.length;
-console.log("Category: " + categoryOne);
-console.log(categoryOneArrayResult);
-
-// Two // 
-const categoryTwo = categories[1].querySelector("h2").textContent;
-const categoryTwoUl = categories[1].querySelectorAll("li");
-let categoryTwoArray = [];
-categoryTwoUl.forEach(function (category) {
-    categoryTwoArray.push(category);
-});
-let categoryTwoArrayResult = "Elements: " + categoryTwoArray.length;
-console.log("Category: " + categoryTwo);
-console.log(categoryTwoArrayResult);
-
-// Three // 
-const categoryThree = categories[2].querySelector("h2").textContent;
-const categoryThreeUl = categories[2].querySelectorAll("li");
-let categoryThreeArray = [];
-categoryThreeUl.forEach(function (category) {
-    categoryThreeArray.push(category);
-});
-let categoryThreeArrayResult = "Elements: " + categoryThreeArray.length;
-console.log("Category: " + categoryThree);
-console.log(categoryThreeArrayResult);
-
